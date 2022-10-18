f3aa1eff82e936d9165d5a9f52a0d4e421cfff62
IaamSteve <kerekesistvan@turr.hu>
2022-10-18 22:20:37 +0200

    Végre átveszi az adatokat, de nem írja ki jól

--- js/script.js
+++ js/script.js
@@ -8,20 +8,27 @@ app.controller('mainCtrl', function($scope){
     //$scope.inTotal = 0;
     $scope.newData = {};
 
-    $scope.data = angular.fromJSON(window.localStorage.getItem('shoplist'));
-    if($scope.data == null){
+    $scope.data = angular.fromJson(window.localStorage.getItem('shoplist'))
+        if($scope.data == null){
         $scope.data = [];
     }
-
+    
     $scope.add = function(){
         if ($scope.newData.product == null || $scope.newData.quantity == null || $scope.newData.price == null || $scope.newData.category == null) {
             $scope.message = 'You left something empty...';  
         } else{
             $scope.data.push({ID: $scope.data.length + 1, product: $scope.data.product, quantity: $scope.data.quantity, price: $scope.data.price, category: $scope.data.category});
-            window.localStorage.setItem('shoplist', angular.toJSON($scope.data));
+            window.localStorage.setItem('shoplist', angular.toJson($scope.data));
             $scope.newData = {};
         }
     }
+    $scope.sum = function(){
 
+    }
+    $scope.delete = function(ID) {
+        let idx = $scope.newData.findIndex(item => item.ID == ID);
+        $scope.data.splice(idx, 1);
+        window.localStorage.setItem('shoplist', angular.toJson($scope.newData));
+    }
 
 })
